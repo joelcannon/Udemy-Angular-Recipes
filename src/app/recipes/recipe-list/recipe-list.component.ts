f260281e807f3c298f9f286f2118b7617794594d
Joel Cannon <joelcannon@mac.com>
2024-05-02 21:20:17 -0500

    add recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://photos-cdn.catchmyparty.com/BLC/uploads/2018/02/fried-rice-14.jpg'
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is simply a test',
+      'https://photos-cdn.catchmyparty.com/BLC/uploads/2018/02/fried-rice-14.jpg'
+    ),
+  ];
 
   constructor() {}
 
