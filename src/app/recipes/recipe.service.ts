1f72b7a8509661899f8a6d07cf32244a4aab0f6d
Joel Cannon <joelcannon@mac.com>
2024-05-22 13:28:35 -0500

    add ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,30 @@
 import { Subject } from 'rxjs';
 import { Recipe } from './recipe.model';
-
+import { Ingredient } from '../shared/ingredient.model';
 export class RecipeService {
   recipeSelected = new Subject<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://photos-cdn.catchmyparty.com/BLC/uploads/2018/02/fried-rice-14.jpg'
+      'Cheeseburger',
+      'A cheeseburger is a hamburger topped with cheese. Traditionally, the slice of cheese is placed on top of the meat patty, but the burger can include many variations in structure, ingredients, and composition. The cheese is normally added to the cooking hamburger patty shortly before serving, which allows the cheese to melt.',
+      'https://upload.wikimedia.org/wikipedia/commons/4/4d/Cheeseburger.jpg',
+      [
+        new Ingredient('ground beef', 1),
+        new Ingredient('cheese', 1),
+        new Ingredient('hamburger bun', 1),
+      ]
     ),
     new Recipe(
-      'Another Test Recipe',
-      'This is simply a test',
-      'https://photos-cdn.catchmyparty.com/BLC/uploads/2018/02/fried-rice-14.jpg'
+      'Hotdog',
+      'A hot dog is a food consisting of a grilled or steamed sausage served in the slit of a partially sliced bun. It can also refer to the sausage itself. The sausage used is a wiener (Vienna sausage) or a frankfurter (Frankfurter Würstchen, also just called frank). The names of these sausages also commonly refer to their assembled dish.',
+      'https://upload.wikimedia.org/wikipedia/commons/f/fb/Hotdog_-_Evan_Swigart.jpg',
+      [
+        new Ingredient('weiner', 1),
+        new Ingredient('mustard', 1),
+        new Ingredient('relish', 1),
+        new Ingredient('hotdog bun', 1),
+      ]
     ),
   ];
 
