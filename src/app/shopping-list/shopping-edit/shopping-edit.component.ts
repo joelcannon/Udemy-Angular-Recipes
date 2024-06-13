608690ae1d2f71d5f63b5680855560f8ed0e17c8
Joel Cannon <joelcannon@mac.com>
2024-06-12 19:51:06 -0500

    refactor using template driven form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Ingredient } from '../../shared/ingredient.model'; // Add this import statement
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -8,21 +9,19 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrl: './shopping-edit.component.css',
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
-
   constructor(private slService: ShoppingListService) {}
 
   ngOnInit(): void {
     let dummy: number = 1; // resolve lint error
   }
 
-  onAddItem(event: Event) {
-    event.preventDefault();
+  onSubmit(form: NgForm) {
+    console.log(form.value); // {name: ..., amount: ...}
+  }
 
-    const ingredientName = this.nameInputRef.nativeElement.value;
-    const ingredientAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingredientName, ingredientAmount);
+  onAddItem(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.slService.addIngredient(newIngredient);
   }
 
