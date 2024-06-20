446fa877882e9673c866cefa8388759dd01b2921
Joel Cannon <joelcannon@mac.com>
2024-06-19 19:09:23 -0500

    store and fetch recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,1 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService
+  ) {}
+
+  storeRecipes() {
+    this.http
+      .put(
+        'https://ng-course-recipe-book-d831a-default-rtdb.firebaseio.com/recipes.json',
+        this.recipeService.getRecipes()
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+    this.http
+      .get<
+        Recipe[]
+      >('https://ng-course-recipe-book-d831a-default-rtdb.firebaseio.com/recipes.json')
+      .subscribe((recipes) => {
+        this.recipeService.setRecipes(recipes);
+        console.log(recipes);
+      });
+  }
+}
+
 // https://ng-course-recipe-book-d831a-default-rtdb.firebaseio.com/
